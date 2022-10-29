c30be4432b7ebe78581a0a27447c8a66d48ee5b0
msxim_m <maximmaksimchyk@gmail.com>
2022-10-29 11:28:13 +0300

    add code

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-try:
+dev(maximum(a, b))
+    if a>b:
+        print("a")
+    elif a<b:
+        print("b")
+
 a = int(input("Введіть число->"))
+b = int(input("Введіть число->"))
+print(maximum(a, b))
